e18caa8cd7d4e51d65cb7bdb9eb67ec23d33c2a6
Noman Ejaz <nomanejaz132@gmail.com>
2024-01-17 18:05:28 +0500

    prettier config for tailwind classes sorting

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center gap-5">
-      <Button variant="outline">Button</Button>
+    <div className="flex h-screen w-screen items-center justify-center gap-5">
+      <Button
+        variant="outline"
+        className="rounded-full bg-violet-500 px-5 py-2 text-sm font-semibold leading-5 text-white hover:bg-violet-600 hover:text-white focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700"
+      >
+        Button
+      </Button>
       <Button
         variant="destructive"
         className="px-2"
