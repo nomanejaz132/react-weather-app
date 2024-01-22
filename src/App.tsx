b6b8884fbb74e36a0a3bd6a0723fe4c8123c702a
Noman Ejaz <nomanejaz132@gmail.com>
2024-01-22 18:13:40 +0500

    msetting ui layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import useWeatherData from "./hooks/useWeatherData";
 import { formatSunTime } from "./lib/dateTimeUtils";
 import { DEFAULT_LOCATION } from "./lib/config";
+import { Card } from "@/components/ui/card";
 
 function App() {
   const [location] = useState(DEFAULT_LOCATION.coordinates);
@@ -9,23 +10,20 @@ function App() {
   const { weatherData, isFetchingWeather } = useWeatherData(location);
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center gap-5">
+    <div className="container mx-auto flex min-h-screen flex-col px-[1rem] py-4 antialiased selection:bg-black selection:text-white dark:bg-black dark:selection:bg-white dark:selection:text-black md:px-[2rem]">
+      <h2 className="pb-5 text-4xl text-blue-700">Weatherman</h2>
       {!isFetchingWeather && (
-        <div>
-          <h2 className="text-4xl text-blue-700">Weatherman</h2>
-          <p className="text-base text-black">
+        <div className="grid grid-cols-5 gap-4">
+          <Card className="text-base text-black">
             Temp: {weatherData?.current?.temp}&deg;
-          </p>
-          <p className="text-base text-black">
-            Feels like: {weatherData?.current?.feels_like}&deg;
-          </p>
-          <p className="text-base text-black">
-            Highest: {weatherData?.current?.max_temp}&deg;
-          </p>
-          <p className="text-base text-black">
+            <br /> Feels like: {weatherData?.current?.feels_like}&deg;
+            <br /> Highest: {weatherData?.current?.max_temp}&deg;
+            <br /> Lowest: {weatherData?.current?.min_temp}&deg;
+          </Card>
+          <Card className="text-base text-black">
             Wind: {weatherData?.current?.wind_speed}
-          </p>
-          <p className="flex text-base text-black">
+          </Card>
+          <Card className="flex text-base text-black">
             Wind Degree: {weatherData?.current?.wind_deg}{" "}
             <div
               className="flex flex-col"
@@ -35,36 +33,36 @@ function App() {
             >
               !
             </div>
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Sunrise:{" "}
             {formatSunTime(
               weatherData?.current?.sunrise,
               weatherData?.timezone_offset,
             )}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Sunset:{" "}
             {formatSunTime(
               weatherData?.current?.sunset,
               weatherData?.timezone_offset,
             )}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             UV Index: {weatherData?.current?.uvi * 10}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Precipitation: {weatherData?.current?.rain?.["1h"]}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Humidity: {weatherData?.current?.humidity}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Visibility: {weatherData?.current?.visibility / 1000}
-          </p>
-          <p className="text-base text-black">
+          </Card>
+          <Card className="text-base text-black">
             Pressure: {weatherData?.current?.pressure}
-          </p>
+          </Card>
         </div>
       )}
     </div>
