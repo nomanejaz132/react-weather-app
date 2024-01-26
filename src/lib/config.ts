89e54e26e97e444e57af06de22d7600caac05e61
Noman Ejaz <nomanejaz132@gmail.com>
2024-01-26 19:10:10 +0500

    icons added

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,32 +3,41 @@ import { Location } from "@/lib/types";
 export const DEFAULT_LOCATION: Location = {
   city: "Islamabad",
   coordinates: {
-    lat: "33.6844202",
-    lon: "73.04788479999999",
-
+    // lat: "33.6844202",
+    // lon: "73.04788479999999",
     // berlin
     // lat: "52.52000659999999",
     // lon: "13.404954",
-
     // dublin
     // lat: "53.3498053",
     // lon: "-6.2603097",
-
     //sydney
     // lat: "33.8688197",
     // lon: "151.2092955",
-
     // lisbon
-    // lat: "38.7222524",
-    // lon: "-9.1393366",
-
+    // lat: "38.71666667",
+    // lon: "-9.13444444",
     // sao paulo
     // lat: "-23.5475493",
     // lon: "-46.6358888",
-
     // hamburg
     // lat: "53.5488282",
     // lon: "9.987170299999999",
+    // san francisco
+    // lat: "37.828724",
+    // lon: "-122.355537",
+    // hawaii
+    // lat: "19.741755",
+    // lon: "-155.844437",
+    // bali
+    // lat: "-8.3627585",
+    // lon: "115.1354983",
+    // mawsynram
+    // lat: "25.297536",
+    // lon: "91.582636",
+    // tutendo
+    lat: "5.744606",
+    lon: "-76.540002",
   },
 };
 
